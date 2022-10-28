4957d45764c18b6e2a29b16b36467304dcaa10a5
vitaliirabin <vitalvabischevich@gmail.com>
2022-10-28 14:10:43 +0300

    logic work

--- app/src/main/java/com/example/aviator/PlayFragment.kt
+++ app/src/main/java/com/example/aviator/PlayFragment.kt
@@ -1,24 +1,38 @@
 package com.example.aviator
 
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
 
 class PlayFragment : Fragment() {
+
+    private lateinit var playViewModel: PlayViewModel
 
     private lateinit var rocketView: ImageView
     private lateinit var pointsView: TextView
     private lateinit var constraintLayoutPlay: ConstraintLayout
     private lateinit var rocketAnimation: Animation
+    private lateinit var scoreView: TextView
+    private lateinit var timeView: TextView
+    private lateinit var bidView: TextView
+    private lateinit var bidPlusButton: ImageButton
+    private lateinit var bidMinusButton: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        playViewModel = ViewModelProviders.of(this).get(PlayViewModel::class.java)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,24 +47,88 @@ class PlayFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         rocketView = view.findViewById(R.id.rocket_view)
         pointsView = view.findViewById(R.id.points)
+        timeView = view.findViewById(R.id.time_textView)
+        scoreView = view.findViewById(R.id.score_count)
+        bidView = view.findViewById(R.id.bid_count)
+        bidPlusButton = view.findViewById(R.id.button_bid_plus)
+        bidMinusButton = view.findViewById(R.id.button_bid_minus)
+        bidView.text = (50).toString()
+        scoreView.text = playViewModel.getScore(context).toString()
         constraintLayoutPlay = view.findViewById(R.id.constraint_layout_play)
         view.findViewById<Button>(R.id.button_play).setOnClickListener {
-            startPlayAnimation()
+            play()
+        }
+        bidPlusButton.setOnClickListener {
+            addBid()
+        }
+        bidMinusButton.setOnClickListener {
+            decreaseBid()
         }
     }
 
-    fun startPlayAnimation() {
+    override fun onDestroy() {
+        playViewModel.setScore(context, scoreView.text.toString().toInt())
+        super.onDestroy()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun startPlayAnimation(): Long {
         rocketAnimation = AnimationUtils.loadAnimation(context, R.anim.rocket_anim)
         val time = (3000..10000).random().toLong()
         val valueAnimator = ValueAnimator.ofInt("0".toInt(), time.toInt())
         valueAnimator.duration = time
         valueAnimator.addUpdateListener {
-            pointsView.text = (it.getAnimatedValue().toString().toFloat() / 1000).toString()
+            timeView.text =
+                "Время: ${
+                    String.format(
+                        "%.2f",
+                        (it.getAnimatedValue().toString().toFloat() / 1000)
+                    )
+                }"
+            pointsView.text =
+                String.format(
+                    "%.2f",
+                    ((it.getAnimatedValue().toString().toFloat()) / 4000)
+                )
         }
         rocketAnimation.duration = time
+        rocketAnimation.interpolator = AccelerateInterpolator()
         rocketView.startAnimation(rocketAnimation)
+        valueAnimator.interpolator = AccelerateInterpolator()
         valueAnimator.start()
-
+        return time
     }
 
+    private fun addBid() {
+        var bid = bidView.text.toString().toInt()
+        bid += 50
+        bidView.text = bid.toString()
+    }
+
+    private fun decreaseBid() {
+        var bid = bidView.text.toString().toInt()
+        if (bid > 50) {
+            bid -= 50
+            bidView.text = bid.toString()
+        } else Toast.makeText(context, "Минимальная ставка 50", Toast.LENGTH_LONG).show()
+    }
+
+    private fun play() {
+        var score = scoreView.text.toString().toInt()
+        val bid = bidView.text.toString().toInt()
+        if (score == 0) {
+            Toast.makeText(context, "У вас на балансе неосталось очков!", Toast.LENGTH_LONG).show()
+        } else {
+            if (score >= bid) {
+                score -= bid
+            } else {
+                Toast.makeText(context, "У вас на балансе недостаточно очков!", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+        val time = startPlayAnimation()
+        score += ((bid * time / 4000).toInt())
+        android.os.Handler().postDelayed({ scoreView.text = score.toString() }, time)
+        playViewModel.setScore(context, score)
+    }
 }
---
+++ app/src/main/java/com/example/aviator/PlayViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.aviator
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+
+private const val SHARED_QUERY = "shared_prefs"
+private const val SCORE = "score"
+
+class PlayViewModel : ViewModel() {
+
+    fun getScore(context: Context?): Int? {
+        val sharedPrefs = context?.getSharedPreferences(SHARED_QUERY, Context.MODE_PRIVATE)
+        if (sharedPrefs?.getInt(SCORE, 0) == 0) {
+            sharedPrefs.edit()?.putInt(SCORE, 10000)?.apply()
+        }
+        return sharedPrefs?.getInt(SCORE, 10000)
+    }
+
+    fun setScore(context: Context?, score: Int) {
+        val sharedPrefs = context?.getSharedPreferences(SHARED_QUERY, Context.MODE_PRIVATE)
+        sharedPrefs?.edit()?.putInt(SCORE, score)?.apply()
+    }
+}
